05fc3361432372ff83e084ba5769b7ccc5d7874d
a. Mukoma <ahmukoma@gmail.com>
2023-06-30 17:25:11 -0400

    added find-index mini project

---
+++ find-index/find-findindex.js
@@ -0,0 +1,45 @@
+/* 
+Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
+findUserByUsername(users, 'taco') // undefined
+*/
+
+
+function findUserByUsername(usersArray, username) {
+    let obj = usersArray.find(function(obj, key, arr){
+        return obj["username"] === username;
+    });
+    
+    return obj;
+}
+
+/*
+Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+removeUser(users, 'akagen') // {username: 'akagen'}
+removeUser(users, 'akagen') // undefined
+*/
+
+function removeUser(usersArray, username) {
+    let removing = usersArray.find(function(obj, index, arr){
+        if (obj["username"] === username){
+            usersArray.splice(index, 1);
+        }
+        return obj["username"] === username;
+    });
+    
+    return removing;
+}
