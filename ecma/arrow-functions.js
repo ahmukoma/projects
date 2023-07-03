4e7fc2441488ceb14e7364803ed6649c722bda6a
a. Mukoma <ahmukoma@gmail.com>
2023-07-02 21:41:54 -0400

    added arrow-functions.js

---
+++ ecma/arrow-functions.js
@@ -0,0 +1,29 @@
+/*1. es5 map callback*/
+function double(arr) {
+  return arr.map(function(val) {
+    return val * 2;
+  });
+}
+
+/*ES2015*/
+function double(arr){
+    return arr.map((val) => {
+        return val * 2;
+    })
+}
+
+/*2. replace all functions*/
+
+function squareAndFindEvens(numbers){
+  var squares = numbers.map(function(num){
+    return num ** 2;
+  });
+  var evens = squares.filter(function(square){
+    return square % 2 === 0;
+  });
+  return evens;
+}
+
+/*ES2015*/
+
+let mySquareAbdFindEvens = numbers => numers.map(num => num * num).filter(evens => evens % 2 === 0);
