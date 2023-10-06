e11280e523f748a09cd561d9ad481b52b98d96ab
a. Mukoma <ahmukoma@gmail.com>
2023-10-05 20:54:41 -0400

    stacks-queues

---
+++ stacks-queues/stack.js
@@ -0,0 +1,61 @@
+/** Node: node for a stack. */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** Stack: chained-together nodes where you can
+ *  remove from the top or add to the top. */
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  /** push(val): add new value to end of the stack. Returns undefined. */
+
+  push(val) {
+      let node = new Node(val);
+      this.last.next = node;
+      this.last = node;
+      this.size = this.size + 1;
+  }
+
+  /** pop(): remove the node from the top of the stack
+   * and return its value. Should throw an error if the stack is empty. */
+
+  pop() {
+      let end = this.last;
+      let node = this.first;
+      while(node){
+          if (node.next){
+              if(!node.next.next){
+                  node.next = null;
+              }
+          }
+          
+          node = node.next;
+      }
+      this.size = this.size - 1;
+      return end;
+  }
+
+  /** peek(): return the value of the first node in the stack. */
+
+  peek() {
+      return this.first;
+  }
+
+  /** isEmpty(): return true if the stack is empty, otherwise false */
+
+  isEmpty() {
+      return this.size === 0;
+  }
+}
+
+module.exports = Stack;
