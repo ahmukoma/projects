bcd5294730d46cf7e9ecfcdd6526d77d6f67039b
a. Mukoma <ahmukoma@gmail.com>
2023-10-20 18:47:08 -0400

    modified function

--- recursion.js
+++ recursion.js
@@ -9,8 +9,8 @@ function product(nums) {
 
 function longest(words) {
     let curr = words[0].length;
-    if(words.length === 1) return curr;
-    
+    if (words.length === 1) return curr;
+
     let long = longest(words.slice(1));
     return longest(words.slice(1)) > curr ? long : curr;
 }
@@ -26,13 +26,14 @@ function everyOther(str) {
 
 function isPalindrome(str) {
     if (str.length === 1 || str.length === 0) return true;
-    return str[0] === str.slice(-1) === isPalindrome(str.slice(1, -1));
+    let initFin = str[0] === str.slice(-1);
+    return initFin === isPalindrome(str.slice(1, -1));
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
 function findIndex(arr, val) {
-    
+
 }
 
 /** revString: return a copy of a string, but in reverse. */
@@ -46,15 +47,15 @@ function revString(str) {
 
 function gatherStrings(obj) {
     let vals = [];
-    
-    for(let key in obj){
-        if(typeof(obj[key]) === 'object'){
+
+    for (let key in obj) {
+        if (typeof (obj[key]) === 'object') {
             vals.push(...gatherStrings(obj[key]));
-        }else if (typeof(obj[key]) === 'string'){
+        } else if (typeof (obj[key]) === 'string') {
             vals.push(obj[key])
         }
     }
-    
+
     return vals;
 }
 
@@ -66,12 +67,12 @@ function binarySearch(arr, val) {
 }
 
 module.exports = {
-  product,
-  longest,
-  everyOther,
-  isPalindrome,
-  findIndex,
-  revString,
-  gatherStrings,
-  binarySearch
+    product,
+    longest,
+    everyOther,
+    isPalindrome,
+    findIndex,
+    revString,
+    gatherStrings,
+    binarySearch
 };
